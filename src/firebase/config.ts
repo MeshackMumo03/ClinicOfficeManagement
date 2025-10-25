f7fc6db0fbdcd449287b3e87b6fec2574296c902
Meshack Mumo <metrem03@gmail.com>
2025-10-25 23:20:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6972381914-5ae46",
+  "appId": "1:786306753771:web:e2b90ed346fa7adf5ccaae",
+  "apiKey": "AIzaSyDv3oQjGbpsDbCXTCHt4uPkTQe8iSzh_GE",
+  "authDomain": "studio-6972381914-5ae46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "786306753771"
+};

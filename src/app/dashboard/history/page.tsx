b307022d43c4569fb54082d0d5167fa7ac14ac2c
Meshack Mumo <metrem03@gmail.com>
2025-12-03 21:38:06 +0000

    Try fixing this error: `Runtime ReferenceError: useEffect is not defined

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useCollection, useFirestore, useUser, useDoc, useMemoFirebase } from "@/firebase";
 import { collection, query, where, orderBy, doc } from "firebase/firestore";
 import { Loader } from "@/components/layout/loader";
